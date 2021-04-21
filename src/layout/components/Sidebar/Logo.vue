d4b6903bb287af5330d0a9a5380940b24af47e85
Xia, Mao-cong <69415869+leftlevel@users.noreply.github.com>
2021-04-21 12:36:45 +0800

    update: 修改图标

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -25,7 +25,7 @@ export default {
   data() {
     return {
       title: 'Gblog博客后台系统',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: require('../../../assets/site-logo.svg')
     }
   }
 }
